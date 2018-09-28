dced1f8635438a4924fe5b9f2c60915806ae3f1c
Loafus <eth.fish@gmail.com>
2018-09-27 18:31:01 -0700

    lil fixies

--- javascript/main.js
+++ javascript/main.js
@@ -41,14 +41,19 @@ $("#submit-button").on("click", function (event) {
 database.ref().on("child_added", function (snapshot) {
 
     if (snapshot.child("name").exists() && snapshot.child("dest").exists() && snapshot.child("firstTime") && snapshot.child("freq")) {
-        
+
         name = snapshot.val().name;
         dest = snapshot.val().dest;
         firstTime = snapshot.val().firstTime;
         freq = parseInt(snapshot.val().freq);
 
-        var minutesAway = freq - (moment().diff(moment(firstTime, "HH:mm"), "minutes") % freq)
-        var nextArrival = moment().add(minutesAway, "m").format("hh:mm a")
+        if (freq === 0) {
+            minutesAway = "Undefined"
+            nextArrival = "Never"
+        } else {
+            var minutesAway = freq - (moment().diff(moment(firstTime, "HH:mm"), "minutes") % freq)
+            var nextArrival = moment().add(minutesAway, "m").format("hh:mm a")
+        }
 
         $("#tbody").append("<tr> <td>" + name + "</td> <td>" + dest + "</td> <td>" + freq + "</td> <td>" + nextArrival + "</td> <td>" + minutesAway + "</td> </tr>");
     }
